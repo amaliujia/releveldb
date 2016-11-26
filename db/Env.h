fd674fd98d9f93715b5218003cde92b3d3ad38f1
amaliujia <amaliujia@163.com>
2016-11-26 12:07:15 -0800

    Finish definition of virtual class: Env

--- db/Env.h
+++ db/Env.h
@@ -27,6 +27,47 @@ public:
   virtual ~Env();
 
   static Env* Default();
+
+  virtual Status NewSequentialFile(const std::string& filename,
+                                    SequentialFile** result) = 0;
+  virtual Status NewRandomAccessFile(const std::string& filename,
+                                      RandomAccessFile** result) = 0;
+  virtual Status NewWritableFile(const std::string& filename,
+                                  WritableFile** result) = 0;
+  virtual Status NewAppendableFile(const std::string& filename,
+                                  WritableFile** result) = 0;
+  virtual bool FileExists(const std::string& filename) = 0;
+
+  virtual Status GetChildren(const std::string& dir,
+                            std::vector<std::string>* result) = 0;
+  virtual Status DeleteFile(const std::string& filename) = 0;
+  virtual Status CreateDir(const std::string& dir) = 0;
+  virtual Status DeleteDir(const std::string& dir) = 0;
+  virtual Status GetFileSize(const std::string& filename, uint64_t* file_size) = 0;
+  virtual Status RenameFile(const std::string& src, const std::string& target) = 0;
+  virtual Status LockFile(const std::string& filename, FileLock** locker) = 0;
+  virtual Status UnlockFile(FileLock* lock) = 0;
+
+  // Arrange to run "(*function)(arg)" once in a background thread.
+  //
+  // "function" may run in an unspecified thread.  Multiple functions
+  // added to the same Env may run concurrently in different threads.
+  // I.e., the caller may not assume that background work items are
+  // serialized.
+  virtual void Schedule(
+  void (*function)(void* arg),
+  void* arg
+  ) = 0;
+
+  virtual void StartThread(void (*function)(void* arg), void* arg) = 0;
+
+  virtual Status GetTestDirectory(std::string* path) = 0;
+
+  virtual Status NewLogger(const std::string& logName, Logger** result) = 0;
+
+  virtual uint64_t NowMicros() = 0;
+
+  virtual void SleepForMicroseconds(int micros) = 0;
 };
 
 class RandomAccessFile {
@@ -129,6 +170,67 @@ extern Status WriteStringToFile(Env* env, const Slice& data,
 extern Status ReadFileToString(Env* env, const std::string& fname,
                                std::string* data);
 
+
+class EnvWrapper : public Env {
+public:
+  // Initialize an EnvWrapper that delegates all calls to *t
+  explicit EnvWrapper(Env* t) : target_(t) { }
+  virtual ~EnvWrapper();
+
+  // Return the target to which this Env forwards all calls
+  Env* target() const { return target_; }
+
+  // The following text is boilerplate that forwards all methods to target()
+  Status NewSequentialFile(const std::string& f, SequentialFile** r) {
+    return target_->NewSequentialFile(f, r);
+  }
+  Status NewRandomAccessFile(const std::string& f, RandomAccessFile** r) {
+    return target_->NewRandomAccessFile(f, r);
+  }
+  Status NewWritableFile(const std::string& f, WritableFile** r) {
+    return target_->NewWritableFile(f, r);
+  }
+  Status NewAppendableFile(const std::string& f, WritableFile** r) {
+    return target_->NewAppendableFile(f, r);
+  }
+  bool FileExists(const std::string& f) { return target_->FileExists(f); }
+  Status GetChildren(const std::string& dir, std::vector<std::string>* r) {
+    return target_->GetChildren(dir, r);
+  }
+  Status DeleteFile(const std::string& f) { return target_->DeleteFile(f); }
+  Status CreateDir(const std::string& d) { return target_->CreateDir(d); }
+  Status DeleteDir(const std::string& d) { return target_->DeleteDir(d); }
+  Status GetFileSize(const std::string& f, uint64_t* s) {
+    return target_->GetFileSize(f, s);
+  }
+  Status RenameFile(const std::string& s, const std::string& t) {
+    return target_->RenameFile(s, t);
+  }
+  Status LockFile(const std::string& f, FileLock** l) {
+    return target_->LockFile(f, l);
+  }
+  Status UnlockFile(FileLock* l) { return target_->UnlockFile(l); }
+  void Schedule(void (*f)(void*), void* a) {
+    return target_->Schedule(f, a);
+  }
+  void StartThread(void (*f)(void*), void* a) {
+    return target_->StartThread(f, a);
+  }
+  virtual Status GetTestDirectory(std::string* path) {
+    return target_->GetTestDirectory(path);
+  }
+  virtual Status NewLogger(const std::string& fname, Logger** result) {
+    return target_->NewLogger(fname, result);
+  }
+  uint64_t NowMicros() {
+    return target_->NowMicros();
+  }
+  void SleepForMicroseconds(int micros) {
+    target_->SleepForMicroseconds(micros);
+  }
+private:
+  Env* target_;
+};
 
 }  // namespace releveldb
 
---
+++ db/Env_posix.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 王 瑞 on 2016/11/26.
+//
+
+#include "db/Env.h"
+
+namespace releveldb{
+
+}  // namespace releveldb
