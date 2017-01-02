80e02d483182efec3cc4e2b319bd762567b58b38
amaliujia <amaliujia@163.com>
2017-01-01 18:49:28 -0800

    Add DBFormat test

---
+++ tests/DBFormatTest/DBFormatTests.cpp
@@ -0,0 +1,13 @@
+//
+// Created by amaliujia on 2016/11/24.
+//
+
+#include "db/random.h"
+#include "db/DBFormat.h"
+#include "gtest/gtest.h"
+
+class DBFormatTests : public ::testing::Test { };
+
+
+TEST(DBFormatTests, Empty) {
+}
